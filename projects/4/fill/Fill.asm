6308238f1355a0e20922153e2e5f8775d16714bd
Edward Palmer <891364+elp2@users.noreply.github.com>
2024-12-12 15:27:21 -0800

    P4 - Fill.

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,3 +8,57 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+
+// Set fill to zero
+@fill
+M=0
+
+(READKEYBOARD)
+  // Read the keyboard
+  @KBD
+  D=M
+  @FILLBLACK
+  D;JNE
+
+(FILLWHITE)
+  @fill
+  M=0
+  @FILLSCREENSETUP
+  0;JEQ
+
+(FILLBLACK)
+  @fill
+  M=-1
+
+  // Fill = 0xFF if keyboard pressed, else 0x00
+(FILLSCREENSETUP)
+  // @pixel = SCREEN
+  @SCREEN
+  D=A
+  @pixel
+  M=D
+
+(SCREENLOOP)
+  // M[@pixel] = @fill
+  @fill
+  D=M
+  @pixel
+  A=M
+  M=D
+  
+  // @pixel += 1
+  @pixel
+  M=M+1
+  D=M
+
+  @KBD
+  D=A-D
+  @READKEYBOARD
+  D;JEQ
+
+  @SCREENLOOP
+  0;JEQ
+  
+  // Otherwise, continue filling loop.
+  @READKEYBOARD
+  0;JMP
