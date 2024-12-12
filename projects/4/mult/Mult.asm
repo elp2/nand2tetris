d865f189ee5587a010d0cce02e80a93e1f34fa55
Edward Palmer <891364+elp2@users.noreply.github.com>
2024-12-12 13:22:47 -0800

    P4 - Mult.

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -6,4 +6,43 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+
+  // R2 = 0
+  @2
+  M=0
+
+  // R3 = R1
+  @1
+  D=M
+  @3
+  M=D
+
+// If R3 == 0, out result should be good in R2
+(LOOP)
+  // D must be R3 at this point
+  @RETURN
+  D;JEQ
+
+  // R2 += R0
+  @2
+  D=M
+  @0
+  D=D+M
+  @2
+  M=D
+
+  // R3 -= 1
+  @3
+  D=M
+  D=D-1
+  M=D
+
+  // Go back to the LOOP
+  @LOOP
+  0;JMP
+
+(RETURN) // INFINITE LOOP
+  @RETURN
+  0;JMP
+
+// End
